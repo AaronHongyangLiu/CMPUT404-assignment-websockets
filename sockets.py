c732a88591781e5a6581d16bffe902b5f6cc52a9
Aaron <supermanaaronliu@gmail.com>
2017-03-18 23:01:05 -0600

    update

--- sockets.py
+++ sockets.py
@@ -123,23 +123,29 @@ def flask_post_json():
 @app.route("/entity/<entity>", methods=['POST','PUT'])
 def update(entity):
     '''update the entities via this interface'''
-    return None
+    body = flask_post_json()
+    for key in body:
+        myWorld.update(entity,key,body[key])
+    if request.method == 'PUT':
+        return flask.jsonify(body)
+    return flask.jsonify(myWorld.world())
 
 @app.route("/world", methods=['POST','GET'])    
 def world():
     '''you should probably return the world here'''
-    return None
+    return flask.jsonify(myWorld.world())
 
 @app.route("/entity/<entity>")    
 def get_entity(entity):
     '''This is the GET version of the entity interface, return a representation of the entity'''
-    return None
+    return flask.jsonify(myWorld.get(entity))
 
 
 @app.route("/clear", methods=['POST','GET'])
 def clear():
     '''Clear the world out!'''
-    return None
+    myWorld.clear()
+    return flask.jsonify(myWorld.world())
 
 
 
